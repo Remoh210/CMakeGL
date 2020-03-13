3a6c4d3cf7ca3f0b5c464e30723ea2f118f7b582
Bogdan <bondarenkobogdankiev@gmail.com>
2020-03-12 21:13:39 -0700

    camera pos

--- src/Main.cpp
+++ src/Main.cpp
@@ -32,7 +32,7 @@ int SceneViewHeight = SCR_HEIGHT * SceneViewScale;
 int SceneViewWidth = SCR_WIDTH * SceneViewScale;
 
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 8.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
